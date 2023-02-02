c4683941d8d510652060571bf5934d4985802b56
Matt Yard <mattyard11@gmail.com>
2023-02-02 14:22:34 -0500

    reconfigures amplify deploy

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -11,7 +11,7 @@ export type AmplifyDependentResourcesAttributes = {
         }
     },
     "storage": {
-        "s3a490de65": {
+        "s3ac9310f3": {
             "BucketName": "string",
             "Region": "string"
         }
